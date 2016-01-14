6f646a6f4d098faf4b023bfc09890173d3509ac1
marbiru <code@uribram.com>
2016-01-14 12:57:20 +0700

    feat: first working canvas version
    non-canvas version still on page; need to re-draw canvas on mouse
    click; need to make presentational improvements; need to make the image
    into an active link

--- main.js
+++ main.js
@@ -12,8 +12,6 @@ $( "#submit" ).click(function () {
 
 	var days_old = Math.floor((today_days - birthday)/mil_day);
 
-	days_old_output.innerHTML = days_old;
-
 for (var counter = 10000; ; counter += 10000 ) {
   if (days_old < counter) {
     big_milestone = counter;
@@ -21,10 +19,17 @@ for (var counter = 10000; ; counter += 10000 ) {
     };
 };
 
+// old, pre-canvas version 
+
+days_old_output.innerHTML = days_old;
+
 big_milestone_output.innerHTML = big_milestone;
 
 big_milestone_date = new Date(birthday + mil_day*big_milestone);
 
+bm_date_final = big_milestone_date.toUTCString().substring(0, 16);
+
+big_milestone_date_output = big_milestone_date.toUTCString().substring(0, 16);
 // should this next line be jQuery?
 big_milestone_date_output.innerHTML = big_milestone_date.toUTCString().substring(0, 16);
 
@@ -57,10 +62,41 @@ var small_milestone_array = [
 
   small_milestone_date = new Date(birthday + mil_day*small_milestone);
 
+  sm_date_final = small_milestone_date.toUTCString().substring(0, 16);
+
   small_milestone_date_output.innerHTML = small_milestone_date.toUTCString().substring(0, 16);
 
   var share_link = "<a href='http://marbiru.github.io/days/share.html" + "?b=" + birthday + "'>here</a>";
 
   share_link_output.innerHTML = share_link;
+
+// new canvas version starts here
+
+var example = document.getElementById('example');
+var context = example.getContext('2d');
+context.font = "40px sans-serif";
+context.fillStyle= "#000";
+context.textAlign = 'center';
+context.fillText("Today I am " + days_old + " days old", 700, 40);
+
+context.font = "40px sans-serif";
+context.fillStyle= "#000";
+context.textAlign = 'center';
+context.fillText("UPCOMING MILESTONES", 700, 100);
+
+context.font = "40px sans-serif";
+context.fillStyle= "#000";
+context.textAlign = 'center';
+context.fillText("I will be " + small_milestone + " days old on " + sm_date_final , 700, 160);
+
+context.font = "40px sans-serif";
+context.fillStyle= "#000";
+context.textAlign = 'center';
+context.fillText("I will be " + big_milestone + " days old on " + bm_date_final , 700, 220);
+
+context.font = "40px sans-serif";
+context.fillStyle= "#000";
+context.textAlign = 'center';
+context.fillText("How about you? Find out at www.marbiru.com/days", 700, 280);
 
 });
